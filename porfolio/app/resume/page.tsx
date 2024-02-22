5b363ba02bb7644f6eb490fdf38690405a4c7dfb
lenoxnguyen2014 <lenoxnguyen2014@gmail.com>
2024-02-22 00:01:46 -0800

    resume render

--- porfolio/app/resume/page.tsx
+++ porfolio/app/resume/page.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image"
+
 const MyResume = () => {
+    // add style for this page tmr
+    // and checkout download button
     return (
-        <div>My Resume </div>
+    <div >
+        <Image
+        src="/resume.svg"
+        alt="Resume 2024"
+        width={80}
+        height={50}
+        priority
+    />
+        </div>
     )
 }
 

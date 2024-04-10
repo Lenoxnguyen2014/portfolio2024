f1e19647e6f804b6e394d0990d8547cf512da725
lenoxnguyen2014 <lenoxnguyen2014@gmail.com>
2024-04-10 01:32:08 -0700

    update bg

--- porfolio/app/layout.tsx
+++ porfolio/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import clouds from'@/public/clouds.svg'
 import rain from '@/public/rain.svg'
 import snow from '@/public/snow.svg'
 import sunny from '@/public/sunny.svg'
@@ -20,7 +21,7 @@ const changeBackgroundBaseOnWeather = async() => {
   const img = (weatherMain: string) => {
     switch (weatherMain) {
     case "Clouds":
-      return snow
+      return clouds
     case "Rain":
       return rain
     case "Snow":

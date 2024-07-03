5a1d31110a38d56f28f715cb470d1c8cfcde3534
lenoxnguyen2014 <lenoxnguyen2014@gmail.com>
2024-07-03 16:07:32 -0700

    animate only once

--- porfolio/components/porfolio.tsx
+++ porfolio/components/porfolio.tsx
@@ -7,22 +7,23 @@ import Image from 'next/image'
 
 export default function Porfolio () {
   return (
+    <div>
     <motion.div
       className='flex items-center w-full h-full 1/2 bg-cyan-900'
       animate={{
         scale: [1, 2, 2, 1, 1],
         rotate: [0, 0, 180, 180, 0],
-        borderRadius: ['0%', '0%', '50%', '50%', '0%']
+        borderRadius: ['0%', '0%', '50%', '50%', '0%'],
       }}
       transition={{
-        duration: 2,
+        duration: 1,
         ease: 'easeInOut',
         times: [0, 0.2, 0.5, 0.8, 1],
-        repeat: Infinity,
-        repeatDelay: 1
+        delay: 1
       }}
     >
-    <Image width={500} height={500} alt="lezen logo" src={lezenio}/>
-</motion.div>
+      <Image width={500} height={500} alt="lezen logo" src={lezenio}/>
+    </motion.div>
+    </div>
   )
 }

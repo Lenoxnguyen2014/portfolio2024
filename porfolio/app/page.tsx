07bc9b428b7017c8b32477c1d779420bf9111187
lenoxnguyen2014 <lenoxnguyen2014@gmail.com>
2024-08-21 19:22:03 -0700

    update resume and add more time

--- porfolio/app/page.tsx
+++ porfolio/app/page.tsx
@@ -8,7 +8,7 @@ const AboutMePreview = lazy(() => delayForDemo(import('../components/aboutMe')))
 
 function delayForDemo(promise: Promise<typeof import("../components/aboutMe")>) {
   return new Promise(resolve => {
-    setTimeout(resolve, 6000);
+    setTimeout(resolve, 600000);
   }).then(() => promise);
 }
 export default async function Home () {

05853cbb6283d4c969bb297b7e4fc054802388fb
lenoxnguyen2014 <lenoxnguyen2014@gmail.com>
2024-08-31 22:59:32 -0700

    responsive process

--- porfolio/components/processIntro.tsx
+++ porfolio/components/processIntro.tsx
@@ -24,10 +24,8 @@ export default function ProcessIntro (props: processIntro) {
         }
         listProcess.push(itemObject)
     })
-
+    console.log(listProcess)
     const sortedProcessList = listProcess.sort((a, b) => (a.step > b.step) ? 1 : -1)
-    const rightProcessList = sortedProcessList.filter((item) => item.step % 2 == 0)
-    const leftProcecssList = sortedProcessList.filter((item) => item.step %2 != 0)
     return (
         <div className="mt-24 flex items-center flex-col w-full h-auto ">
             <div className="flex items-center justify-center md:mx-8">
@@ -38,30 +36,36 @@ export default function ProcessIntro (props: processIntro) {
                     </span>
                 </h1>
             </div>
-            <div className="mt-24 grid grid-cols-2 gap-2 max-sm:grid-cols-1">
+            <div className="mt-24 grid grid-cols-2 gap-2 max-md:grid-cols-1">
                 {sortedProcessList.map((item, key) => {
                     if (item.step %2 == 0) {
                         return(
-                            <div className="w-[500px] mt-[24rem] ml-[-64px] py-8 text-primary ">
-                                <div className="border w-16 h-16 p-6 rounded-full flex items-center border-secondary">
+                            <div className="w-[500px] mt-[24rem] ml-[-64px] py-8 text-primary max-sm:w-96 max-sm:block">
+                                <div className="border w-16 h-16 p-6 rounded-full flex items-center border-secondary bg-slate-700">
                                     {item.step}
                                 <div className="border border-dashed border-slate-700 h-[600px] opacity-90"></div>
                                 </div>
-                                <div className=" w-full p-6 ml-20 mt-[-16px] border border-secondary">
-                                        {item.title}
-                                        { documentToReactComponents(item.describeStep, renderOptions) }
+                                <div className=" w-full p-6 ml-[72px] mt-[-30px] border border-secondary">
+                                        <h2 className="text-lg bold">{item.title}</h2>
+                                        <h4 className="text-2xl">{item.subTitle}</h4>
+                                        <p className="mt-4">
+                                            { documentToReactComponents(item.describeStep, renderOptions) }
+                                        </p>
                                 </div>
                             </div>
                         )
                     } else {
                         return(
-                            <div className="w-[500px] h-64 py-8 mr-16 text-primary">
-                                <div className="border w-16 h-16 p-6 float-right mr-[-72px] rounded-full flex items-center border-secondary">
+                            <div className="w-[500px] h-64 py-8 mr-16 text-primary max-sm:w-96 max-sm:block">
+                                <div className="border w-16 h-16 p-6 float-right mr-[-72px] rounded-full flex items-center border-secondary max-md:ml-[-72px] max-md:float-left">
                                     {item.step}
                                 </div>
-                                <div className="w-full p-8 mt-8 border border-secondary">
-                                        {item.title}
+                                <div className="w-full p-8 mt-6 border border-secondary">
+                                    <h4 className="text-lg bold">{item.title}</h4>
+                                    <h2 className="text-2xl">{item.subTitle}</h2>
+                                    <p className="mt-4">
                                         { documentToReactComponents(item.describeStep, renderOptions) }
+                                    </p>
                                 </div>
 
                             </div>

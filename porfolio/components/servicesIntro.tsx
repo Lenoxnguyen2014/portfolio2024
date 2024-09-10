add4860e839e6f471f2556c955ef05cb960a53a7
lenoxnguyen2014 <lenoxnguyen2014@gmail.com>
2024-09-09 20:46:52 -0700

    update service intro responsive

--- porfolio/components/servicesIntro.tsx
+++ porfolio/components/servicesIntro.tsx
@@ -25,12 +25,12 @@ export default function ServicesIntro (props: introServices) {
     })
 
     return (
-        <div className="grid grid-cols-3 gap-2 mt-24 md:flex flex-cols flex-wrap max-sm:block">
+        <div className="grid grid-cols-3 gap-2 mt-24 md:flex flex-cols flex-wrap max-sm:block max-sm:mt-6">
             {isClient ? 
             <>
                 {listServices.reverse().map((item, key) => {
                 return(
-                    <div key={key} className="border border-secondary w-[400px] h-[280px] p-6 ">
+                    <div key={key} className="border border-secondary w-[400px] h-[280px] p-6 max-sm:w-[80vw] max-sm:h-auto max-sm:mb-6 ">
                         <h2 className="bold text-2xl text-secondary mb-4">{item.name}</h2>
                         <div className="text-primary mt-12 font-sans text-xl">{documentToReactComponents( item.intro, renderOptions)} </div>
                     </div>
